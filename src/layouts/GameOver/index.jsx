174fcca8b04b9898f70a6acbd866a1dc86090a2d
emanuelecardone <e_m.cardone@yahoo.it>
2022-07-23 22:27:05 +0100

    Messo if else per gameover ultimo lv

--- src/layouts/GameOver/index.jsx
+++ src/layouts/GameOver/index.jsx
@@ -5,17 +5,28 @@ import { DataContext } from '../../App';
 
 const GameOver = () => {
 
+    const datas = useContext(DataContext);
+    const [gameData] = datas;
+    const {levels: {list}, user: {currentLevel}} = gameData;
+    let nextLvButton;
+    let finalMessage;
     // Il button "next" viene mostrato se non ci si trova all'ultimo livello
-    
+    if(currentLevel !== list.length - 1){
+        nextLvButton = <Button message='next' sections={['gameOver', 'game']} change={true} />
+        finalMessage = 'You won! Congrats!';
+    } else{
+        nextLvButton = null;
+        finalMessage = 'Amazing, you completed the game!';
+    }
 
     // Il gameover dà la possibilità di rigiocare il livello appena finito
     // oppure di passare al successivo
     return(
         <div className='gameover custom_section'>
-            <h2 className='gamover_title'>You won! Congrats!</h2>
+            <h2 className='gamover_title'>{finalMessage}</h2>
             <div className='buttons_wrapper d-flex justify-content-center align-items-center'>
                 <Button message='Restart' sections={['gameOver', 'game']} />
-                <Button message='next' sections={['gameOver', 'game']} change={true} />
+                {nextLvButton}
             </div>
         </div>
     );
